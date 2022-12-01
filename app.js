92631c145d068bbe195f561420cd33c31c83bb1a
Jamienorthman <mikoann88@gmail.com>
2022-12-01 17:25:07 +0100

    added function and comments

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
-const compChoices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    return compChoices[Math.floor(Math.random() * 3)];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function checkWinner(playerSelection, computerSelection) {
+    let ps = playerSelection;
+    let cs = computerSelection;
+    if (cs === ps) {
+        return "Draw";
+    } else if (ps === 'rock'     && cs === 'paper' || 
+    ps === 'scissors' && cs === 'rock' ||
+    ps === 'paper'    && cs === 'scissors') {
+        return "Computer";
+    } else {
+        return "Player";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
     let ps = playerSelection.toLowerCase();
     let cs = computerSelection.toLowerCase();
-    if (cs === ps) {
+    if (result === "Draw") {
         return "It's a draw!";
-    } else if (ps === 'rock'     && cs === 'paper' || 
-               ps === 'scissors' && cs === 'rock' ||
-               ps === 'paper'    && cs === 'scissors') {
+    } else if (result === "Computer") {
         return `You lose! ${cs} beats ${ps}.`;
     } else {
         return `You win! ${ps} beats ${cs}.`;
     } 
 }
-// Check for valid choice ??
-//else if (ps !== 'rock' || ps !== 'paper' || ps !== 'scissors') {
-        //return "Enter a valid choice.";
 
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        let playerChoice = prompt("Enter Rock, Paper, or Scisors: ");
-        let computerChoice = getComputerChoice();
-        console.log(`Round ${i}: ${playRound(playerChoice, computerChoice)}`);
-    }   
+function getPlayerChoice() {
+    let validatedInput = false;
+    while (validatedInput === false) {
+        let playerChoice = prompt("Enter Rock, Paper, or Scissors: ");
+        if (playerChoice === null) {
+            continue;
+        }
+        const playerChoiceLower = playerChoice.toLowerCase();
+        if (choices.includes(playerChoiceLower)) {
+            validatedInput = true;
+            return playerChoiceLower;
+        }
+    }
 }
 
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    for (let i = 1; i <= 5; i++) {
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        console.log(`Round ${i}: ${playRound(playerChoice, computerChoice)}`);
+        console.log("---------------");
+        if (checkWinner(playerSelection, computerSelection) === "Player") {
+            scorePlayer++;
+        } else if (checkWinner(playerSelection, computerSelection) === "Computer") {
+            scoreComputer++;
+        }
+    }  
+    console.log("GAME OVER");
+    if (scorePlayer > scoreComputer) {
+        console.log("Player WINS!");
+    } else if (scorePlayer < scoreComputer) {
+        console.log("Computer WINS!");
+    } else {
+        console.log("Tie GAME!");
+    }
+}
 
-function keepScore(playerScore, computerScore) {
-    game()
- }
+// game();
 
-game();
-
-// const playerSelection = 'rock';
-// add input field or prompt for player
 // then give points
-
-
-//function game() {
-//    playRound(playerSelection, computerSelection)
-//}
